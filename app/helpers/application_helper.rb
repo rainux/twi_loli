468c253b2cf7e401fa74053510aa89288e650352
Rainux Luo <rainux@gmail.com>
2010-03-07 03:08:58 +0800

    Refactor #format_tweet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,22 +5,35 @@ module ApplicationHelper
 
   def format_tweet(tweet)
     tweet = tweet.gsub /\s{2,}/, ' '
-    tweet = tweet.gsub %r{[0-9A-Za-z]{2,4}://([0-9A-Za-z_-]+\.)+[0-9A-Za-z]*[0-9A-Za-z/+=%&_.~?:\[\]-]*} do |url|
-      short_url = truncate(url)
-      link_to short_url, url, :target => '_blank', :title => url, 'data-truncated' => (url != short_url)
-    end
-    tweet = tweet.gsub %r{@[0-9A-Za-z_/]+} do |user|
-      user = user[1..-1]
-      '@' + link_to(user, user_path(user))
-    end
-    tweet = tweet.gsub %r{#\w+} do |tag|
-      link_to tag, search_path(:q => tag[1..-1])
-    end
+    tweet = auto_link_usernames(tweet)
+    tweet = auto_link_tags(tweet)
+    tweet = auto_link_urls(tweet)
   end
 
   def extract_mentioned_users(tweet)
     tweet.scan(%r{@[0-9A-Za-z_]+}).collect do |user|
       user[1..-1]
+    end
+  end
+
+  private
+  def auto_link_urls(tweet)
+    tweet.gsub %r{[0-9A-Za-z]{2,4}://([0-9A-Za-z_-]+\.)+[0-9A-Za-z]*[0-9A-Za-z/+=%&_.~?:\[\]-]*} do |url|
+      short_url = truncate(url)
+      link_to short_url, url, :target => '_blank', :title => url, 'data-truncated' => (url != short_url)
+    end
+  end
+
+  def auto_link_usernames(tweet)
+    tweet.gsub %r{@[0-9A-Za-z_/]+} do |user|
+      user = user[1..-1]
+      '@' + link_to(user, user_path(user))
+    end
+  end
+
+  def auto_link_tags(tweet)
+    tweet.gsub %r{#\w+} do |tag|
+      link_to tag, search_path(:q => tag[1..-1])
     end
   end
 end
