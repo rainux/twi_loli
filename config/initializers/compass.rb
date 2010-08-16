0e3e3501013b257ef9a0325200b624a1f5640926
Rainux Luo <rainux@gmail.com>
2010-08-17 06:52:17 +0800

    Fixed compass configuration

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,5 @@
 require 'compass'
 # If you have any compass plugins, require them here.
-Compass.configuration.parse(Rails.root.join('config', 'compass.config'))
+Compass.add_project_configuration(Rails.root.join('config', 'compass.config'))
 Compass.configuration.environment = Rails.env.to_sym
 Compass.configure_sass_plugin!
