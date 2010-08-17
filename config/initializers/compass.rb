f502946f434dab58ce3e5bac3d7439a29799844f
Rainux Luo <rainux@gmail.com>
2010-08-17 10:38:51 +0800

    Compass upgraded

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,3 @@
 require 'compass'
-# If you have any compass plugins, require them here.
-Compass.add_project_configuration(Rails.root.join('config', 'compass.config'))
-Compass.configuration.environment = Rails.env.to_sym
-Compass.configure_sass_plugin!
+require 'compass/app_integration/rails'
+Compass::AppIntegration::Rails.initialize!
