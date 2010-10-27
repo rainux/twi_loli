a06d7eeb0d10841ecd4471c96a7fddfe9aef8c12
Rainux Luo <rainux@gmail.com>
2010-10-27 20:21:28 +0800

    Session expire after 2 weeks

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-TwiLoli::Application.config.session_store :cookie_store, :key => '_twi_loli_session'
+TwiLoli::Application.config.session_store :cookie_store,
+  :key => '_twi_loli_session', :expire_after => 2.weeks
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
